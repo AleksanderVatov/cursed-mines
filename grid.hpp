e3a7eb07420381fe1d7e212d1c21d55053022b68
Alexander Vatov <alvatov@gmail.com>
2020-07-03 14:15:21 +0200

    Adds move constructor and conversions from string and from array

--- grid.hpp
+++ grid.hpp
@@ -18,8 +18,12 @@ public:
         _height = height;
         _width = width;
     }
+    
+    Grid(Grid<Type> &&) = default;
+    
     ~Grid() {
-        delete [] _array;
+        std::cerr << "Deconstructing Grid with _array = " << (void*)_array;
+        if(_array) delete [] _array;
     }
     
     Type & operator()(std::size_t row, std::size_t col) {
@@ -32,6 +36,14 @@ public:
         if(row >= _height || col >= _width)
             throw std::out_of_range("Grid::operator(): Point out of range!");
         return _array[_index(row,col)];
+    }
+    
+    bool operator==(Grid<Type> const & other) const {
+        if (_height != other._height || _width != other._width) return false;
+        for (int i = 0; i < _height*_width; ++i)
+            if(_array[i] != other._array[i])
+                return false;
+        return true;
     }
     
     inline std::size_t width() const {
@@ -54,6 +66,25 @@ public:
         }
         
         return s.str();
+    }
+    
+    static Grid<Type> fromArray(Type array[], std::size_t height, std::size_t width) {
+        Grid<Type> g(height, width);
+        for(int i = 0; i < height*width; ++i) g._array[i] = array[i];
+        return g;
+    }
+    
+    
+    static Grid<Type> fromString(std::string s, std::size_t height, std::size_t width) {
+        std::stringstream ss;
+        ss.str(s);
+        Grid<Type> g(height, width);
+        Type t;
+        for(int i = 0; i < height*width; ++i) {
+            if (ss >> t) g._array[i] = t;
+            else break;
+        }
+        return g;
     }
     
 protected:
--- grid_test.cpp
+++ grid_test.cpp
@@ -4,17 +4,24 @@
 #include "grid.hpp"
 
 BOOST_AUTO_TEST_CASE(General) {
-     BOOST_CHECK_THROW(Grid<char>(3,0), std::invalid_argument);
-     Grid <char> g(2,3);
-     BOOST_TEST(g.width() == 3);
-     BOOST_TEST(g.height() == 2);
-     BOOST_CHECK_THROW(g(5,5), std::out_of_range);
-     g(0,0)='a';
-     g(0,1)='b';
-     g(0,2)='c';
-     g(1,0)='d';
-     g(1,1)='e';
-     g(1,2)='f';
-     BOOST_TEST(g.toString("|") == "a|b|c\nd|e|f");
-     BOOST_TEST(Grid<int>(1,3).toString() == "000"); //Checks correct initialization
+    BOOST_TEST(Grid<char>::fromString("abcdef",2,3).toString() == "abc\ndef");
+    
+    int array1[6] = {1, 2, 3, 4, 5, 6};
+    auto g0 = Grid<int>::fromArray(array1, 2, 3);
+    BOOST_ASSERT(Grid<int>::fromArray(array1, 2, 3) == Grid<int>::fromString("1 2 3 4 5 6", 2, 3));
+    BOOST_TEST(Grid<int>::fromArray(array1, 2, 3).toString() == Grid<int>::fromString("1 2 3 4 5 6", 2, 3).toString());
+
+    BOOST_CHECK_THROW(Grid<char>(3,0), std::invalid_argument);
+    Grid <char> g(2,3);
+    BOOST_TEST(g.width() == 3);
+    BOOST_TEST(g.height() == 2);
+    BOOST_CHECK_THROW(g(5,5), std::out_of_range);
+    g(0,0)='a';
+    g(0,1)='b';
+    g(0,2)='c';
+    g(1,0)='d';
+    g(1,1)='e';
+    g(1,2)='f';
+    BOOST_TEST(g.toString("|","\t") == "a|b|c\td|e|f");
+    BOOST_TEST(Grid<int>(1,3).toString() == "000"); //Checks correct initialization
 }
